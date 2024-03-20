d4e43faa054bc8abd347b6598e9388d7c9f98524
LandewankFF <landewankfahrezaf@gmail.com>
2024-03-20 16:28:08 +0700

    feat: responsive navbar

--- src/Script/Navbar.js
+++ src/Script/Navbar.js
@@ -9,7 +9,18 @@ window.addEventListener("scroll", function () {
 });
 
 // Create resnposive menu
-// const menuOpen = document.querySelector(".menu-open");
-// const menuClose = document.querySelector(".close-menu");
-// const menuList = document.getElementById("menu-list");
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".close-menu");
+const menuList = document.getElementById("menu-list");
+
+menuOpen.addEventListener('click',function(){
+    menuList.classList.add("active")
+    menuOpen.classList.add("hide")
+    menuClose.classList.add("show")
+})
+menuClose.addEventListener('click',function(){
+    menuList.classList.remove("active")
+    menuOpen.classList.remove("hide")
+    menuClose.classList.remove("show")
+})
 
